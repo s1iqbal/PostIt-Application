056a07e37595230df5ff224b5660162b37d8c48b
Saad Iqbal <s1iqbal@ryerson.ca>
2018-01-31 22:13:44 -0500

    List all notes
    display all notes in json file.

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-console.log('Starting app.js');
-
 const fs = require('fs');
 const _ = require('lodash');
 const yargs = require('yargs');
@@ -8,31 +6,38 @@ const notes = require('./notes.js');
 
 const argv = yargs.argv;
 var command = argv._[0];
-console.log('Command: ', command);
-console.log('Yargs', argv);
+
 
 if (command === 'add') {
   var note = notes.addNote(argv.title, argv.body);
   if (note) {
     console.log('Note created');
     notes.logNote(note);
-  } else {
+  }
+  else {
     console.log('Note title taken');
   }
-} else if (command === 'list') {
-  notes.getAll();
-} else if (command === 'read') {
+}
+else if (command === 'list') {
+  var allNotes = notes.getAll();
+  console.log(`Printing ${allNotes.length} note(s).`);
+  allNotes.forEach((note) => notes.logNote(note));
+}
+else if (command === 'read') {
   var note = notes.getNote(argv.title);
   if (note) {
     console.log('Note found');
     notes.logNote(note);
-  } else {
+  }
+  else {
     console.log('Note not found');
   }
-} else if (command === 'remove') {
+}
+else if (command === 'remove') {
   var noteRemoved = notes.removeNote(argv.title);
   var message = noteRemoved ? 'Note was removed' : 'Note not found';
   console.log(message);
-} else {
+}
+else {
   console.log('Command not recognized');
 }
--- notes.js
+++ notes.js
@@ -1,5 +1,3 @@
-console.log('Starting notes.js');
-
 const fs = require('fs');
 
 var fetchNotes = () => {
@@ -31,7 +29,7 @@ var addNote = (title, body) => {
 };
 
 var getAll = () => {
-  console.log('Getting all notes');
+  return fetchNotes();
 };
 
 var getNote = (title) => {
